f99e81468a105013bcf663432531ee65136298ce
Dávid Czirok <d.czirok99@gmail.com>
2021-04-07 22:56:17 +0200

    Added ija libararies

---
+++ get-libs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+wget --no-check-certificate --content-disposition https://github.com/kostyio/IJA-Warehouse-Project/blob/e1f972764b014c4a7f3dc0d165018bba1672c8fe/lib/javafx11.zip -O javafx11.zip
+wget https://github.com/kostyio/IJA-Warehouse-Project/raw/master/lib/ant-javafx.jar -O ant-javafx.jar
+wget https://github.com/kostyio/IJA-Warehouse-Project/raw/master/lib/jfxrt.jar -O jfxrt.jar
+wget https://github.com/kostyio/IJA-Warehouse-Project/raw/master/lib/jackson-annotations-2.12.2.jar -O jackson-annotations-2.12.2.jar
+wget https://github.com/kostyio/IJA-Warehouse-Project/raw/master/lib/jackson-core-2.12.2.jar -O jackson-core-2.12.2.jar
+wget https://github.com/kostyio/IJA-Warehouse-Project/raw/master/lib/jackson-databind-2.12.2.jar -O jackson-databind-2.12.2.jar
